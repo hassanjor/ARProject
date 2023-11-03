5d9c318be57f4403a6eedbbdf9bd33801da70ba9
hassan joraid <113706390+hassanjor@users.noreply.github.com>
2023-11-03 12:56:18 -0400

    Handling 2 players
    2 players can now play and works

--- Assets/Scripts/Player2.cs
+++ Assets/Scripts/Player2.cs
@@ -1,37 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
-
-public class Player1 : MonoBehaviour
+public class Player2 : Player1
 {
-    //Refrence to animator 
-    public Animator animator;
-    //Refrence to hide rolling the dice button 
-    public Button button;
-    //Board refrence 
-    public Board board;
-    //Dice reference 
-    public Dice dice;
-    //reference to the player object
-    public GameObject player1;
-    //determines if player should be in starting game position
-    public  bool StartGame = true;
-    //Tracks the player current position
-    public int currentPosition = 0;
-    //emulator stuff for lerping 
-    public int interpolationFramesCount = 45; 
-    public int elapsedFrames = 0;
 
-    //manipulates button spawning
-    public bool diceRolled = false;
-    public static bool Player1Turn = true;
-    public static bool Player2Turn = false;
-    //animations references to detect when they are done
-    public string animationName;
-    public string ladderAnimationName;
+  
+    // Start is called before the first frame update
     void Start()
     {
         //deactivate the roll button, and dice
@@ -39,65 +14,14 @@ public class Player1 : MonoBehaviour
         dice.gameObject.SetActive(false);
     }
 
-   public void StartPlayerPosition()
-    {
-        //move player to the first Tile
-        Vector3 worldPosition = board.tileArray[currentPosition % board.width, currentPosition / board.width].transform.position;
-        transform.position = worldPosition;
 
-    }
-
-    public void PlayerMovement()
-    {
-        //Move player depending on the dice rolled value
-        int result = dice.rollValue;
-        //+= adds onto it so it always is saving the position
-        currentPosition += result;
-        //lerping using a value
-        float interpolationRatio = (float)elapsedFrames / interpolationFramesCount;
-        //this considers the width and height of the board that the player cant go off it and will instead go to the next row 
-        Vector3 worldPosition = board.tileArray[currentPosition % board.width, currentPosition / board.width].transform.position;
-        //set is walking to true so the animation plays
-        animator.SetBool("isWalking?", true);
-        // Start a coroutine to smoothly move the player
-        StartCoroutine(MovePlayer(worldPosition));
-
-    }
-
-  public IEnumerator MovePlayer(Vector3 targetPosition)
-    {
-        float elapsedTime = 0f;
-        // Adjust the duration for player to reach target
-        float duration = 1.0f; 
-
-        Vector3 startingPosition = transform.position;
-
-        while (elapsedTime < duration)
-        {
-            transform.position = Vector3.Lerp(startingPosition, targetPosition, elapsedTime / duration);
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-
-        //the final position is exactly the target position
-        transform.position = targetPosition;
-        //back to idle
-        animator.SetBool("isWalking?", false);
-    }
-
-    public void Clicked()
-    {
-        //whenever the roll button is clicked deactivate it 
-         button.gameObject.SetActive(false);
-    }
-
-
-
+    // Update is called once per frame
     void Update()
     {
-        if (Player1Turn == true)
-        {
-            Debug.Log(currentPosition);
+
+        Debug.Log(currentPosition);
+  
+
 
             //If the board was spawned move the player to the first Tile
             //and move the dice to the middle
@@ -111,6 +35,8 @@ public class Player1 : MonoBehaviour
                 dice._rb.position = board.tileArray[5, 5].transform.position + new Vector3(0, 2, 0);
                 diceRolled = true;
             }
+        if (Player2Turn == true)
+        {
             //If board spawned and start already happened and the dice was rolled move the player to the rolled value
             if (board.BoardGenerated && !StartGame && dice.rollDice)
             {
@@ -119,9 +45,8 @@ public class Player1 : MonoBehaviour
                 diceRolled = true;
                 //re position the dice at the middle after
                 dice._rb.position = board.tileArray[5, 5].transform.position + new Vector3(0, 2, 0);
-                Player1Turn = false;
-                Player2Turn = true;
-
+                Player1Turn = true;
+                Player2Turn = false;
             }
             //manipulating button appearing
             if (diceRolled)
@@ -134,8 +59,8 @@ public class Player1 : MonoBehaviour
             //Snake check 
             if (currentPosition == 23)
             {
-                Player1Turn = true;
-                Player2Turn = false;
+                Player1Turn = false;
+                Player2Turn = true;
                 //disable button
                 button.gameObject.SetActive(false);
                 //play the sad animation
@@ -154,18 +79,17 @@ public class Player1 : MonoBehaviour
                     animator.SetBool("Lost?", false);
                     //activate button again
                     button.gameObject.SetActive(true);
-                    Player1Turn = false;
-                    Player2Turn = true;
+                    Player2Turn = false;
+                    Player1Turn = true;
                 }
-               
-
+             
             }
 
             //Snake 2
             if (currentPosition == 55)
             {
-                Player1Turn = true;
-                Player2Turn = false;
+                Player1Turn = false;
+                Player2Turn = true;
                 //disable button
                 button.gameObject.SetActive(false);
                 //play the sad animation
@@ -177,6 +101,7 @@ public class Player1 : MonoBehaviour
                 // Check if the specific animation is over
                 if (stateInfo.IsName(animationName) && stateInfo.normalizedTime >= 1.0f)
                 {
+
                     //move player down to snake tail
                     StartCoroutine(MovePlayer(board.tileArray[6, 3].transform.position));
                     //set current position to the position player was moved to 
@@ -185,18 +110,17 @@ public class Player1 : MonoBehaviour
                     animator.SetBool("Lost?", false);
                     //activate button again
                     button.gameObject.SetActive(true);
-                    Player1Turn = false;
-                    Player2Turn = true;
+                    Player2Turn = false;
+                    Player1Turn = true;
                 }
-              
-
+               
             }
 
             //Ladder check
             if (currentPosition == 7)
             {
-                Player1Turn = true;
-                Player2Turn = false;
+                Player1Turn = false;
+                Player2Turn = true;
                 //disable button
                 button.gameObject.SetActive(false);
                 //play the happy animation
@@ -215,11 +139,10 @@ public class Player1 : MonoBehaviour
                     animator.SetBool("Victory?", false);
                     //activate button again
                     button.gameObject.SetActive(true);
-                    Player1Turn = false;
-                    Player2Turn = true;
+                    Player2Turn = false;
+                    Player1Turn = true;
                 }
-            
-
+               
             }
 
         }
